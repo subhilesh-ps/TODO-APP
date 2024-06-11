1a7f72ad0eac2c37f350296eb6a38dfa90b6ee25
subhilesh-ps <subhilesh96@gmail.com>
2024-06-11 17:22:13 +0530

    complete all the functionalities

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,13 @@
+/* eslint-disable react/no-unescaped-entities */
+import Button from "react-bootstrap/Button";
+
 import "./App.css";
+import logo from "./assets/images/Todo.png";
 import { useState } from "react";
-import { FaTrash } from "react-icons/fa";
+import { Container } from "react-bootstrap";
+import { FaEdit } from "react-icons/fa";
+
+import { IoIosTrash } from "react-icons/io";
 
 function App() {
   const [inputValue, setInputValue] = useState({
@@ -17,6 +24,10 @@ function App() {
   };
 
   const handleClick = () => {
+    if (inputValue.task === "") {
+      alert("no task, add a task");
+      return;
+    }
     if (isEditing) {
       setInputArray((prev) =>
         prev.map((item) =>
@@ -51,32 +62,47 @@ function App() {
   };
 
   return (
-    <>
-      <h1>Daily Task Manager</h1>
-      <input type="text" value={inputValue.task} onChange={handleChange} />
+    <Container>
+      <div className="header">
+        <img src={logo} alt="" style={{ width: "10rem" }} />
+        <h1>TO Do </h1>
+        <p style={{ color: " #7bafdc" }}>Remind Me Every Thing's</p>
+      </div>
+      <input
+        className="input"
+        placeholder="  Enter your Activity"
+        type="text"
+        value={inputValue.task}
+        onChange={handleChange}
+      />
+      <Button variant="outline-success" onClick={handleClick}>
+        {isEditing ? "Update" : "Submit"}
+      </Button>
 
-      <button onClick={handleClick}>{isEditing ? "Update" : "Submit"} </button>
+      <Button variant="outline-danger" onClick={handleClear}>
+        Clear
+      </Button>
+      <h4>Todo List </h4>
+      <div className="footer-box">
+        {inputArray.map((item) => (
+          <div className="footer" key={item.id}>
+            <h3 style={{ paddingRight: "100px" }}>{item.task}</h3>
 
-      <button onClick={handleClear}>Clear</button>
-      {inputArray.map((item) => (
-        <div className="manage" key={item.id}>
-          <h4>{item.task}</h4>
-          <button onClick={() => handleEdit(item.id)}>
-            {" "}
-            Edit
-            {/* <i class="fa-regular fa-pen-to-square"></i> */}
-          </button>
-          <button
-            onClick={() => {
-              handleDelete(item.id);
-            }}
-          >
-            delete
-            <FaTrash style={{ color: "red" }} />
-          </button>
-        </div>
-      ))}
-    </>
+            <FaEdit
+              onClick={() => handleEdit(item.id)}
+              style={{ fontSize: "25px" }}
+            />
+
+            <IoIosTrash
+              style={{ fontSize: "30px" }}
+              onClick={() => {
+                handleDelete(item.id);
+              }}
+            />
+          </div>
+        ))}
+      </div>
+    </Container>
   );
 }
 
